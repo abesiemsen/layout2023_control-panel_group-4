fab462b7e5112afe35a098e7069882c07a82d86d
jeffrey <jeffrey.w@wustl.edu>
2023-03-22 18:30:13 -0500

    final update push

---
+++ control-panel/final/script.js
@@ -0,0 +1,31 @@
+const cards = document.getElementsByClassName('card');
+const extraInfo = document.getElementsByClassName('extra_info');
+
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards);
+    cards[i].addEventListener('click', function () {
+        // Toggle visibility of the corresponding extra info element
+        if (extraInfo[i].style.display === 'block') {
+            extraInfo[i].style.display = 'none';
+        } else {
+            extraInfo[i].style.display = 'block';
+        }
+    });
+    cards[i].addEventListener('mouseover', () => {
+        cards[i].style.cursor = 'pointer';
+        cards[i].style.background = '#ccc';
+    });
+    cards[i].addEventListener('mouseout', () => {
+        cards[i].style.cursor = 'default';
+        cards[i].style.background = '#fff';
+    });
+}
+Sortable.create(stopList, {
+    animation: 100,
+    group: 'list-1',
+    draggable: '.list-group-item',
+    handle: '.list-group-item',
+    sort: true,
+    filter: '.sortable-disabled',
+    chosenClass: 'active'
+});
